eb4db7f65e73b6c0639406ebeb7cc64143fba8ff
duangharutai <duangharutaisaw@gmail.com>
2025-07-15 13:44:25 +0700

    task03 solution

--- task03.js
+++ task03.js
@@ -6,3 +6,37 @@
 // 11+ chars → "Strong"
 
 const password = "pass123";
+
+if (password.length < 6) {
+    console.log("Too short");
+} else if (password.length <=10) {
+    console.log("Moderate");
+} else if(password.length >=11) {
+    console.log("Strong");
+} 
+
+// task03 solution
+// console.log(password.length);
+// if (password.length >= 11) {
+//     console.log("Strong");
+// } else if (password.length >= 6) {
+//     console.log("Moderate");
+// } else {
+//     console.log("Too short");
+// };
+
+// or
+// const password = "1221333333233";
+//     // console.log(password.length);
+// if (password.length < 6) {
+//     // Code to execute if condition is true
+//     console.log("Too short");
+// } else if (password.length >= 6 && password.length <= 10) {
+//     // Code to execute if anotherCondition is true
+//     console.log("Moderate");
+
+// } else {
+//     // Code to execute if all conditions are false
+//     console.log("Strong");
+
+// }
