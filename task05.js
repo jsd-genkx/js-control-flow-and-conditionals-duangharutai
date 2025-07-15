03eb15d698dbc33503b8f649927cacf99c20c9ad
duangharutai <duangharutaisaw@gmail.com>
2025-07-15 14:42:15 +0700

    task05 solution

--- task05.js
+++ task05.js
@@ -2,7 +2,7 @@
 
 // TODO: Convert this if to switch/case (You can add more options.)
 
-const fruit = "banana";
+const fruit = "apple";
 
 if (fruit === "apple") {
 	console.log("An apple a day keeps the doctor away.");
@@ -12,4 +12,18 @@ if (fruit === "apple") {
 	console.log("Oranges are full of vitamin C.");
 } else {
 	console.log("Unknown fruit.");
+}
+
+switch (fruit) {
+    case "apple":
+        console.log("An apple a day keeps the doctor away.");
+        break;
+    case "banana":
+        console.log("Banana is good for potassium");
+        break;
+    case "orange":
+        console.log("Oranges are full of vitamin C.");
+        break;
+    default:
+        console.log("Unknown fruit.");
 }
